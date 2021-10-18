cf4a7e2b27e5bca153a790152848947b5a441419
rehanhussainimam <rehanhussainimam@gmail.com>
2021-10-18 19:44:25 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AngularMaterialModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
