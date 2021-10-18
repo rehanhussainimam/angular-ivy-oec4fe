5437d9ab7414e88b58bce05b1b6af5922b19ad5a
rehanhussainimam <rehanhussainimam@gmail.com>
2021-10-18 19:15:13 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface user {
+  name: string;
+  age: string;
+  edu: string;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public userdata: user[] = [
+    {
+      name: 'Rehan',
+      age: '28',
+      edu: 'Maths',
+    },
+    {
+      name: 'Swami',
+      age: '30',
+      edu: 'Sciencee',
+    },
+    {
+      name: 'Sunil',
+      age: '26',
+      edu: 'History',
+    },
+  ];
+
+  dataSource;
+  displayedColumns = ['name', 'age', 'edu'];
+
+  constructor() {
+    this.dataSource = this.userdata;
+  }
 }
